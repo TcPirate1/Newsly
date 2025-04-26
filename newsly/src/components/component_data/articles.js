93e84dfe3e0280acd591f94ce4a09509602589a7
TC_win_PC <88120195+TcPirate1@users.noreply.github.com>
2025-04-26 14:26:49 +1200

    Adding more feeds to article.js

--- newsly/src/components/component_data/articles.js
+++ newsly/src/components/component_data/articles.js
@@ -5,21 +5,46 @@ export const rnzFeed = [
 ];
 
 export const allFeeds = [
-    {
-        title: "RNZ",
-        link: "https://www.rnz.co.nz/rss/national.xml",
-        category: "news"
-    },
-    {
-        title: "Stuff",
-        link: "https://www.stuff.co.nz/rss",
-        category: "news"
-    },
-    {
-        title: "",
-        link: "",
-        category: ""
-    }
+  {
+    title: "RNZ",
+    link: "https://www.rnz.co.nz/rss/national.xml",
+    category: "news",
+  },
+  {
+    title: "Stuff",
+    link: "https://www.stuff.co.nz/rss",
+    category: "news",
+  },
+  {
+    title: "Christchurch News",
+    link: "https://newsline.ccc.govt.nz/news/stories",
+    category: "news",
+  },
+  {
+    title: "Github",
+    link: "https:/github.blog/changelog/feed/",
+    category: "dev",
+  },
+  {
+    title: "Chocolatey",
+    link: "https://feeds.feedburner.com/ChocolateyBlog",
+    category: "dev",
+  },
+  {
+    title: "VScode",
+    link: "https://code.visualstudio.com/feed.xml",
+    category: "dev",
+  },
+  {
+    title: "Windows",
+    link: "https://blogs.windows.com/feed",
+    category: "tech",
+  },
+  {
+    title: "",
+    link: "",
+    category: "",
+  },
 ];
 
 const parser = new Parser();
